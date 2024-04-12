13facc6dc0616bcf0d00335e77fa6d2aaa15e982
Grogros4 <thibaud.gloaguen@polytechnique.edu>
2024-04-12 09:49:49 +0200

    Added LDA topices + text processing function

--- text_processing.py
+++ text_processing.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
-from data_utils import load_reddit_data
 import torch
 from time import sleep
+import re
+
 
 bnb_config = BitsAndBytesConfig(
 load_in_4bit=True,
@@ -12,6 +13,42 @@ bnb_4bit_use_double_quant=True,
 bnb_4bit_quant_type="nf4",
 bnb_4bit_compute_dtype=torch.bfloat16
 )
+
+
+def remove_urls_from_string(text: str):
+    # Regular expression to identify URLs
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    # Replace URLs with an empty string
+    return url_pattern.sub(r'', text)
+
+
+def remove_url(df: pd.DataFrame, text_column: str):
+    df[text_column] = df[text_column].apply(remove_urls_from_string)
+    return df
+
+def preprocess_data(df: pd.DataFrame):
+    # Pre-processing
+    df = df.dropna(subset = "selftext")
+    df = df[ df["removed"] != 1]
+
+    # convert to datetime
+    df["datetime"] = pd.to_datetime(df["created"], unit="s")\
+
+    # Set 'datetime' column as the index
+    df.set_index('datetime', inplace=True, drop = True)
+    
+    # Remove rBitcoinMod
+    mask = df["author"] != "rBitcoinMod"
+    mask = mask & (df["author"] != "SomeBrokeChump") # This guy also makes sticky thread, see https://www.reddit.com/r/Bitcoin/comments/sozb08/daily_discussion_february_10_2022/
+    
+    # Prepend title to selftext
+    df["selftext"] = df["title"] + "\n" + df["selftext"]
+    
+    # remove urls
+    df = remove_url(df, "selftext")
+    
+    return df[mask]
+    
 
 
 def mistral_relevant_text_identification(df: pd.DataFrame, text_column: str):
@@ -64,7 +101,8 @@ def mistral_relevant_text_identification(df: pd.DataFrame, text_column: str):
 
 if __name__ == "__main__":
     
-    df = load_reddit_data()
+    df = pd.read_csv("bitcoin_2022/submission.csv")
+    df = preprocess_data(df)
     df = mistral_relevant_text_identification(df, "selftext")
     
     df.to_csv("mistral_tagging.csv", index=False)
