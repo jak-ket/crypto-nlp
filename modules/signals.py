df872293a6a232e9ca388d536dd25f4f7c514b31
jak-ket <mynpmail@gmx.de>
2024-05-29 15:54:32 +0200

    add exponential weighting

--- modules/signals.py
+++ modules/signals.py
@@ -1,8 +1,23 @@
 import pandas as pd
 
-def get_sentiment_score(df: pd.DataFrame, lag: str = "24h"):
-    df["rolling_positive_score"] = df["positive_score"].rolling(lag).sum()
-    df["rolling_negative_score"] = df["negative_score"].rolling(lag).sum()
+def get_sentiment_score(df: pd.DataFrame, lag: str, exponential_decay: bool, num_comments_weighting: bool):
+    """lag can be specified as timedelta"""
+
+    def get_rolling(s: pd.Series, exponential_decay=exponential_decay, lag=lag, times=df["datetime"]):
+        if exponential_decay:
+            return s.ewm(halflife=lag, times=times).sum()
+        else:
+            return s.rolling(lag).sum()
+
+    if num_comments_weighting:
+        df["rolling_positive_score"] = get_rolling(df["num_comments"]*df["positive_score"])
+        df["rolling_negative_score"] = get_rolling(df["num_comments"]*df["negative_score"])
+    else:
+        df["rolling_positive_score"] = get_rolling(df["positive_score"])
+        df["rolling_negative_score"] = get_rolling(df["negative_score"])
+    
     df["alpha"] = (df["rolling_positive_score"] - df["rolling_negative_score"]) / (df["rolling_positive_score"] + df["rolling_negative_score"] + 1e-4)
+
     return df
+
 
