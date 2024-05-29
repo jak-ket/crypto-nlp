6f9225df5ceced70b68896266367ae686589277b
Grogros4 <thibaud.gloaguen@polytechnique.edu>
2024-05-29 15:41:11 +0200

    Added btc price data processing

--- modules/data_utils.py
+++ modules/data_utils.py
@@ -1,7 +1,10 @@
 import pandas as pd
+from modules.data_cleaning import preprocess_data
 
 def load_reddit_data():
-    return pd.read_csv("bitcoin_2022/submission.csv")
+    df =  pd.read_csv("bitcoin_2022/submission.csv")
+    df = preprocess_data(df)
+    return df
 
 def load_bitcoin_data():
     return pd.read_csv("bitcoin_2022/bitcoin_hourly.csv")
--- modules/signals.py
+++ modules/signals.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from typing import List
 
 def get_sentiment_score(df: pd.DataFrame, lag: str = "24h"):
     df["rolling_positive_score"] = df["positive_score"].rolling(lag).sum()
@@ -6,3 +10,58 @@ def get_sentiment_score(df: pd.DataFrame, lag: str = "24h"):
     df["alpha"] = (df["rolling_positive_score"] - df["rolling_negative_score"]) / (df["rolling_positive_score"] + df["rolling_negative_score"] + 1e-4)
     return df
 
+def plot_realization(df: pd.DataFrame, alpha: str, perfs: List[str], norm: str = "l1", threshold: float = 0.0):
+
+    sns.set_style('whitegrid')
+    sns.set_palette("Set2")
+    
+    plt.rcParams.update({'font.size': 14, 'axes.labelsize': 16, 'axes.titlesize': 18})
+    
+    fig, ax = plt.subplots(figsize=(12, 6))
+    
+    for perf in perfs:
+        
+        if norm == "l1":
+            df["real"] = np.sign(df[alpha]) * df[perf]
+            df["real"] = df["real"] * (np.abs(df[alpha]) > threshold)
+        elif norm == "l2":
+            beta = df[alpha].cov(df[perf]) / df[alpha].var()
+            df["real"] = beta * df[alpha] * df[perf]
+        
+        # Calculate cumulative sum for the 'real' column
+        df['cumulative_real'] = df['real'].cumsum()
+        
+        # Plot each performance with a label
+        sns.lineplot(data=df, x="datetime", y="cumulative_real", ax=ax, label=f"{perf} ({norm} norm)")
+    
+    # Adding titles and labels
+    ax.set_title("Cumulative Realized Performance")
+    ax.set_ylabel("Cumulative Performance")
+    ax.set_xlabel("Date")
+    ax.legend(title="Performance Metrics")
+    
+    return fig, ax
+
+    
+def compute_key_metrics(df: pd.DataFrame, alpha: str, perfs: List[str]):
+    
+    metrics = {
+        "perf": [],
+        "sharpe": [],
+        "bias": [],
+        "alpha": []
+    }
+    
+    for perf in perfs:
+        
+        beta = df[alpha].cov(df[perf]) / df[alpha].var()
+        
+        metrics["perf"].append(df[perf].sum())
+        metrics["sharpe"].append(df[perf].mean() / df[perf].std())
+        metrics["bias"].append(beta * (df["alpha"] * df["perf"]))
+        metrics["alpha"].append(df[alpha].mean())
+        
+        
+    return metrics
+    
+    
